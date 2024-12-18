98b1f7a7502851f7c2f9f60a921e3a16d85ea392
AlissonK1 <Alisson.kauan77@gmail.com>
2024-12-18 12:49:32 -0300

    Criação da Logica de manter items guardados no LocalStorage, ajustes no design e criação de um botão de deleção

--- src/app/modules/to-do-list/components/input-add-item/input-add-item.component.ts
+++ src/app/modules/to-do-list/components/input-add-item/input-add-item.component.ts
@@ -1,9 +1,13 @@
-import { ChangeDetectorRef, Component, ElementRef, EventEmitter, inject, Output, ViewChild } from '@angular/core';
+import { ChangeDetectorRef, Component, ElementRef, EventEmitter, inject, Input, Output, ViewChild } from '@angular/core';
+
+//Interfaces
+import { IListItems } from '../../interface/IListItems.interface';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-input-add-item',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './input-add-item.component.html',
   styleUrl: './input-add-item.component.scss'
 })
@@ -12,12 +16,26 @@ export class InputAddItemComponent {
 
   @ViewChild("inputText") public inputText!: ElementRef;
 
-  @Output() public outputListItems = new EventEmitter<any>()
+  @Input({ required: true }) public inputListItems : Array<IListItems> = [];
+
+  @Output() public outputAddListItems = new EventEmitter<IListItems>()
   
   public focusAndAddItem(value : string){
     if(value){
       this.#cdr.detectChanges();
-      console.log(value)
+      this.inputText.nativeElement.value = '';
+
+      const currentDate = new Date();
+      const timeStamp = currentDate.getTime();
+      const id = `ID ${timeStamp}`
+
+      this.outputAddListItems.emit({
+        id,
+        checked: false,
+        value
+      })
+      
+      return this.inputText.nativeElement.focus();
     }
   }
 }
